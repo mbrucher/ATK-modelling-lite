f128c414fbbf36528bd94bdd12758a32bb5ebbba
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-03-25 23:06:15 +0100

    Adding the opposite check for RC

--- test/Python/Modeling/Capacitor_test.py
+++ test/Python/Modeling/Capacitor_test.py
@@ -32,3 +32,31 @@ def RC_test():
         print(i)
         model({})
         assert_almost_equal(model.state[2], 1 - math.exp(-i * dt / (R * C)), 1e-4)
+
+
+def RC2_test():
+    R = 1e3
+    C = 1e-3
+    dt = 1e-3
+    
+    model = Modeler(3)
+
+    model.add_component(Voltage(0), [0])
+    Vcc = Voltage(0)
+    model.add_component(Vcc, [1])
+    model.add_component(Resistor(R), [0, 2])
+
+    model.dt = dt
+    model.setup()
+    
+    assert_almost_equal(model.state, [0, 0, 0])
+
+    model.add_component(Capacitor(C), [2, 1])
+    Vcc.V = 1
+
+    model.setup(False)
+
+    for i in range(1000):
+        print(i)
+        model({})
+        assert_almost_equal(model.state[2], math.exp(-i * dt / (R * C)), 1e-4)
