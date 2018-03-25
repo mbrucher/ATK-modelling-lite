1f3e5e3cb32cd00f77876ce71fbffa9639969271
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-03-25 18:13:50 +0100

    Adding 2 new tests

--- test/Python/Modeling/Resistor_test.py
+++ test/Python/Modeling/Resistor_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-from numpy.testing import assert_equal
+from numpy.testing import assert_almost_equal
 
 def SimpleBridgeResistor_test():
     from ATK.Modeling import *
@@ -12,4 +12,29 @@ def SimpleBridgeResistor_test():
     model.add_component(Resistor(100), [0, 2])
     model.add_component(Resistor(200), [2, 1])
 
-    assert_equal(model([]), [0, 5, 5./3])
+    assert_almost_equal(model([]), [0, 5, 5./3])
+
+def ParallelBridgeResistor_test():
+    from ATK.Modeling import *
+    model = Modeler(3)
+
+    model.add_component(Voltage(0), [0])
+    model.add_component(Voltage(5), [1])
+    model.add_component(Resistor(100), [0, 2])
+    model.add_component(Resistor(400), [1, 2])
+    model.add_component(Resistor(400), [2, 1])
+
+    assert_almost_equal(model([]), [0, 5, 5./3])
+
+
+def SeriqlBridgeResistor_test():
+    from ATK.Modeling import *
+    model = Modeler(4)
+
+    model.add_component(Voltage(0), [0])
+    model.add_component(Voltage(5), [1])
+    model.add_component(Resistor(100), [0, 2])
+    model.add_component(Resistor(100), [1, 3])
+    model.add_component(Resistor(100), [3, 2])
+
+    assert_almost_equal(model([]), [0, 5, 5./3, 10./3])
