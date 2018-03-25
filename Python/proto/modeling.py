f956c48fa6b3fe53b666271d2ea9acc2c2da65d2
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-03-25 17:55:00 +0100

    Example that works for modeling pins with 0 state.

--- Python/proto/modeling.py
+++ Python/proto/modeling.py
@@ -5,6 +5,9 @@ from __future__ import print_function
 
 import numpy as np
 
+EPS = 1.e-8
+MAX_ITER = 8
+
 class Voltage(object):
     """
     Class that sets a pin to a specific voltage
@@ -38,7 +41,10 @@ class Resistor(object):
         pass
     
     def get_current(self, pin_index, state):
-        return (state[self.pins[1]] - state[self.pins[0]])/ self.R * (1 if self.pins[0] == pin_index else -1)
+        return (state[self.pins[1]] - state[self.pins[0]]) / self.R * (1 if 0 == pin_index else -1)
+
+    def get_gradient(self, pin_index_ref, pin_index, state):
+        return (1 if 1 == pin_index else -1) * (1 if 0 == pin_index_ref else -1) / self.R
 
 class Modeler(object):
     """
@@ -84,21 +90,53 @@ class Modeler(object):
                 
         self.initialized = True
         
-    def compute_current(self, index, pin):
-        return sum([component.get_current(index, self.state) for (component, index) in pin])
+    def compute_current(self, pin):
+        """
+        Compute Kirschhoff law for the non static pin
+        Compute also the jacobian for all the connected pins
+        """
+        eq = sum([component.get_current(i, self.state) for (component, i) in pin])
+        jac = [0] * len(self.mapping)
+        for (component, j) in pin:
+            for (i, component_pin) in enumerate(component.pins):
+                if not self.static_pins[component_pin]:
+                    jac[self.mapping[component_pin]] += component.get_gradient(j, i, self.state)
+        return eq, jac
     
+    def iterate(self, input):
+        """
+        Do one iteration
+        """        
+        eqs = []
+        jacobian = []
+        for (pin_index, mapped) in self.mapping.items():
+            eq, jac = self.compute_current(self.pins[pin_index])
+            eqs.append(eq)
+            jacobian.append(jac)
+        
+        eqs = np.array(eqs)
+        jacobian = np.array(jacobian)
+        if np.all(np.abs(jacobian)) < EPS:
+            return True
+        
+        delta = np.linalg.solve(jacobian, eqs)
+        if np.all(np.abs(delta)) < EPS:
+            return True
+
+        self.state[self.mapping.keys()] -= delta[self.mapping.values()]
+        return False
+        
     def __call__(self, input):
         """
         Works out the value for the new input vector
         """
         if not self.initialized:
             self.setup()
-        eqs = []
-        for (i, pin) in enumerate(self.pins):
-            if not self.static_pins[i]:
-                eqs.append(self.compute_current(i, pin))
         
-        print(eqs)
+        iteration = 0
+        while iteration < MAX_ITER and not self.iterate(input):
+          iteration = iteration + 1        
+        
         return self.state
 
 if __name__ == "__main__":
@@ -107,9 +145,9 @@ if __name__ == "__main__":
     model.add_component(Voltage(0), [0])
     model.add_component(Voltage(5), [1])
     model.add_component(Resistor(100), [0, 2])
-    model.add_component(Resistor(200), [1, 2])
+    model.add_component(Resistor(200), [2, 1])
   
     print(model)
 
     print(model([]))
-    print(model.mapping)
+    print(model([]))
