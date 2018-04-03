3de9a45e1c481ef20611e220a2ba7e0e84fd2624
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-04-03 22:07:48 +0100

    Fix for compatibility with spice.py

--- Python/proto/ATK/Modeling/active.py
+++ Python/proto/ATK/Modeling/active.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 import numpy as np
 import math
 
-from .modeling import retrieve_voltage
+from modeling import retrieve_voltage
 
 class TransistorNPN(object):
     """
