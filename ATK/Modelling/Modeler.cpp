a8ad42b3620c896de2f61f7b62cdfce545a96cbf
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-04-11 21:59:39 +0100

    Constructor is now done

--- ATK/Modelling/Modeler.cpp
+++ ATK/Modelling/Modeler.cpp
@@ -8,7 +8,7 @@
 namespace ATK
 {
   Modeler::Modeler(gsl::index nb_dynamic_pins, gsl::index nb_static_pins, gsl::index nb_input_pins)
-  :nb_dynamic_pins(nb_dynamic_pins), nb_static_pins(nb_static_pins), nb_input_pins(nb_input_pins), dynamic_pins(nb_dynamic_pins), static_pins(nb_static_pins), input_pins(nb_input_pins)
+  :nb_dynamic_pins(nb_dynamic_pins), nb_static_pins(nb_static_pins), nb_input_pins(nb_input_pins), dynamic_pins(nb_dynamic_pins), static_pins(nb_static_pins), input_pins(nb_input_pins), dynamic_state(nb_dynamic_pins, 0), static_state(nb_static_pins, 0), input_state(nb_input_pins, 0)
   {
   }
   
--- ATK/Modelling/Modeler.h
+++ ATK/Modelling/Modeler.h
@@ -25,6 +25,10 @@ namespace ATK
   
   class ATK_MODELLING_EXPORT Modeler
   {
+  public:
+    typedef double DataType;
+    
+  private:
     gsl::index nb_dynamic_pins;
     gsl::index nb_static_pins;
     gsl::index nb_input_pins;
@@ -33,7 +37,13 @@ namespace ATK
     std::vector<int> static_pins;
     std::vector<int> input_pins;
 
+    std::vector<DataType> dynamic_state;
+    std::vector<DataType> static_state;
+    std::vector<DataType> input_state;
+
     std::unordered_set<std::unique_ptr<Component>> components;
+    
+    bool initialized = false;
   public:
     Modeler(gsl::index nb_dynamic_pins, gsl::index nb_static_pins, gsl::index nb_inputs);
     
