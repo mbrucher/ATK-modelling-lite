f45aa5386839316bcd01f803d2a91c96966d4da2
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-04-11 21:36:31 +0100

    Starting the dynamic modeler

---
+++ ATK/Modelling/Component.cpp
@@ -0,0 +1,12 @@
+/**
+ * \file Component.cpp
+ */
+
+#include "Component.h"
+
+namespace ATK
+{
+  Compnent::~Component()
+  {
+  }
+}
---
+++ ATK/Modelling/Component.h
@@ -0,0 +1,17 @@
+/**
+ * \file Component.h
+ */
+
+#ifndef ATK_MODELLING_COMPONENT_H
+#define ATK_MODELLING_COMPONENT_H
+
+namespace ATK
+{
+  class Component
+  {
+  public:
+    virtual ~Component();
+  };
+}
+
+#endif
