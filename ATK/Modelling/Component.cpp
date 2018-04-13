75c3198e1d5536c667e7c54fc311a18297af6f3f
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-04-13 21:59:03 +0100

    Add full component API

--- ATK/Modelling/Component.cpp
+++ ATK/Modelling/Component.cpp
@@ -18,4 +18,20 @@ namespace ATK
   void Component::update_model(Modeler* modeler)
   {
   }
+  
+  void Component::update_steady_state(DataType dt)
+  {
+  }
+  
+  void Component::update_state()
+  {
+  }
+  
+  void Component::precompute(bool steady_state)
+  {
+  }
+  
+  void Component::add_equation(gsl::index eq_number, bool steady_state)
+  {
+  }
 }
--- ATK/Modelling/Component.h
+++ ATK/Modelling/Component.h
@@ -27,6 +27,9 @@ namespace ATK
     /// Local pins to which this component is connected to
     std::vector<std::tuple<PinType, gsl::index>> pins;
     
+    /// The current modeler where the component is located
+    Modeler* modeler;
+    
   public:
     /// Virtual destructor
     virtual ~Component();
@@ -42,7 +45,47 @@ namespace ATK
      * @param modeler the modeler to update
      */
     virtual void update_model(Modeler* modeler);
+    
+    /**
+     * Update the component for its steady state condition
+     * @param is the modeler from which to retrieve the state
+     * @param dt is the delat that will be used in following updates
+     */
+    virtual void update_steady_state(DataType dt);
+    
+    /**
+     * Update the component for its current state condition
+     * @param is the modeler from which to retrieve the state
+     */
+    virtual void update_state();
 
+    /**
+     * Precompute internal value before asking current and gradients
+     * @param steady_state is a flag to indcate steady state computation (used for some components)
+     */
+    virtual void precompute(bool steady_state);
+
+    /**
+     * Get current for the given pin based on the state
+     * @param pin_index is the pin from which to compute the current
+     * @param steady_state is a flag to indcate steady state computation (used for some components)
+     */
+    virtual DataType get_current(gsl::index pin_index, bool steady_state) = 0;
+    
+    /**
+     * Get current gradient for the given pins based on the state
+     * @param pin_index_ref is the pin of the current from which the gradient is computed
+     * @param pin_index is the pin from which to compute the gradient of the pin_index current
+     * @param steady_state is a flag to indcate steady state computation (used for some components)
+     */
+    virtual DataType get_gradient(gsl::index pin_index_ref, gsl::index pin_index, bool steady_state) = 0;
+    
+    /**
+     * Add a new equation to the modeler
+     * @param eq_number is the equation number to add
+     * @param steady_state is a flag to indcate steady state computation (used for some components)
+     */
+    virtual void add_equation(gsl::index eq_number, bool steady_state);
   };
 }
 
