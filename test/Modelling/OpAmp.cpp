8899e2a8506694f60b69751f8ca6aeccafbd6853
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-04-28 15:27:13 +0100

    Adding non inverting test (and doubling the output)

--- test/Modelling/OpAmp.cpp
+++ test/Modelling/OpAmp.cpp
@@ -45,3 +45,32 @@ BOOST_AUTO_TEST_CASE( OpAmp_Follower )
     BOOST_CHECK_CLOSE(data[i], model.get_output_array(0)[i], 0.0001);
   }
 }
+
+BOOST_AUTO_TEST_CASE( OpAmp_DoubleNonInverter )
+{
+  std::array<double, PROCESSSIZE> data;
+  for(gsl::index i = 0; i < PROCESSSIZE; ++i)
+  {
+    data[i] = std::cos(i);
+  }
+  
+  ATK::InPointerFilter<double> generator(data.data(), 1, PROCESSSIZE, false);
+  generator.set_output_sampling_rate(48000);
+  
+  ATK::ModellerFilter<double> model(2, 1, 1);
+  model.set_input_sampling_rate(48000);
+  model.set_output_sampling_rate(48000);
+  
+  model.add_component(std::make_unique<ATK::OpAmp<double>>(), {{std::make_tuple(ATK::PinType::Dynamic, 0), std::make_tuple(ATK::PinType::Input, 0), std::make_tuple(ATK::PinType::Dynamic, 1)}});
+  model.add_component(std::make_unique<ATK::Resistor<double>>(100), {{std::make_tuple(ATK::PinType::Static, 0), std::make_tuple(ATK::PinType::Dynamic, 0)}});
+  model.add_component(std::make_unique<ATK::Resistor<double>>(100), {{std::make_tuple(ATK::PinType::Dynamic, 1), std::make_tuple(ATK::PinType::Dynamic, 0)}});
+
+  model.set_input_port(0, &generator, 0);
+  
+  model.process(PROCESSSIZE);
+  
+  for(gsl::index i = 0; i < PROCESSSIZE; ++i)
+  {
+    BOOST_CHECK_CLOSE(2 * data[i], model.get_output_array(1)[i], 0.0001);
+  }
+}
