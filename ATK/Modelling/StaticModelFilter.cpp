3c50c770e732033216720003b8a8935c68c7b9c4
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-05-11 23:29:02 +0100

    Adding link directory Seems like on macOS, LLVM config is not complete :/

---
+++ ATK/Modelling/StaticModelFilter.cpp
@@ -0,0 +1,11 @@
+/**
+ * \file StaticModelFilter.h
+ */
+
+#include <llvm/Support/Host.h>
+
+#include "StaticModelFilter.h"
+
+namespace ATK
+{
+}
---
+++ ATK/Modelling/StaticModelFilter.h
@@ -0,0 +1,11 @@
+/**
+ * \file StaticModelFilter.h
+ * Contains the static generator
+ */
+
+#include <memory>
+#include <string>
+
+namespace ATK
+{
+}
