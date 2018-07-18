49b3b82bea72be403be37275e12585bc33046100
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-07-18 22:46:41 +0100

    OK, let's simplify the calls by using what popencl uses, but still seems that it doesn't work as advertised.

--- ATK/Modelling/StaticModelFilter.cpp
+++ ATK/Modelling/StaticModelFilter.cpp
@@ -4,6 +4,8 @@
 
 #ifdef ENABLE_CLANG_SUPPORT
 
+#include <sstream>
+
 #include <clang/AST/ASTContext.h>
 #include <clang/AST/ASTConsumer.h>
 #include <clang/Basic/DiagnosticOptions.h>
@@ -16,6 +18,7 @@
 #include <clang/Basic/TargetInfo.h>
 #include <clang/CodeGen/CodeGenAction.h>
 #include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/CompilerInvocation.h>
 #include <clang/Frontend/TextDiagnosticPrinter.h>
 #include <clang/Lex/HeaderSearch.h>
 #include <clang/Lex/HeaderSearchOptions.h>
@@ -24,6 +27,7 @@
 #include <clang/Parse/ParseAST.h>
 #include <clang/Sema/Sema.h>
 
+#include <llvm/InitializePasses.h>
 #include <llvm/ExecutionEngine/ExecutionEngine.h>
 #include <llvm/ExecutionEngine/SectionMemoryManager.h>
 #include <llvm/IR/LLVMContext.h>
@@ -38,6 +42,35 @@
 namespace
 {
   std::unique_ptr<llvm::ExecutionEngine> EE;
+  bool LLVMinit = false;
+  
+  void InitializeLLVM()
+  {
+    if (LLVMinit)
+    {
+      return;
+    }
+    // We have not initialized any pass managers for any device yet.
+    // Run the global LLVM pass initialization functions.
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
+    llvm::InitializeNativeTargetAsmParser();
+
+    auto& Registry = *llvm::PassRegistry::getPassRegistry();
+    
+    llvm::initializeCore(Registry);
+    llvm::initializeScalarOpts(Registry);
+    llvm::initializeVectorization(Registry);
+    llvm::initializeIPO(Registry);
+    llvm::initializeAnalysis(Registry);
+    llvm::initializeIPO(Registry);
+    llvm::initializeTransformUtils(Registry);
+    llvm::initializeInstCombine(Registry);
+    llvm::initializeInstrumentation(Registry);
+    llvm::initializeTarget(Registry);
+    
+    LLVMinit = true;
+  }
 }
 
 namespace ATK
@@ -56,6 +89,8 @@ namespace ATK
   template<typename Function>
   Function parseString(const std::string& fullfile, const std::string& function)
   {
+    InitializeLLVM();
+    
     clang::DiagnosticOptions diagnosticOptions;
     std::unique_ptr<clang::TextDiagnosticPrinter> textDiagnosticPrinter =
       std::make_unique<clang::TextDiagnosticPrinter>(llvm::outs(),
@@ -65,65 +100,49 @@ namespace ATK
     std::unique_ptr<clang::DiagnosticsEngine> diagnosticsEngine =
       std::make_unique<clang::DiagnosticsEngine>(diagIDs, &diagnosticOptions, textDiagnosticPrinter.get());
     
-    clang::LangOptions languageOptions;
-    clang::FileSystemOptions fileSystemOptions;
-    clang::FileManager fileManager(fileSystemOptions);
-    clang::SourceManager sourceManager(*diagnosticsEngine,
-                                       fileManager);
-    std::shared_ptr<clang::HeaderSearchOptions> headerSearchOptions(new clang::HeaderSearchOptions());
+    clang::CompilerInstance compilerInstance;
+    auto& compilerInvocation = compilerInstance.getInvocation();
     
-    const std::shared_ptr<clang::TargetOptions> targetOptions = std::make_shared<clang::TargetOptions>();
-    targetOptions->Triple = llvm::sys::getDefaultTargetTriple();
+    std::stringstream ss;
+    ss << "-triple=" << llvm::sys::getDefaultTargetTriple();
 
-    std::unique_ptr<clang::TargetInfo> targetInfo(
-      clang::TargetInfo::CreateTargetInfo(*diagnosticsEngine, targetOptions));
-
-    clang::HeaderSearch headerSearch(headerSearchOptions,
-                                     sourceManager,
-                                     *diagnosticsEngine,
-                                     languageOptions,
-                                     targetInfo.get());
-    clang::MemoryBufferCache PCMCache;
-    clang::CompilerInstance compInst;
-
-    std::shared_ptr<clang::PreprocessorOptions> opts(std::make_shared<clang::PreprocessorOptions>());
-    clang::Preprocessor preprocessor(opts,
-                                     *diagnosticsEngine,
-                                     languageOptions,
-                                     sourceManager,
-                                     PCMCache,
-                                     headerSearch,
-                                     compInst);
-    preprocessor.Initialize(*targetInfo);
+    std::istream_iterator<std::string> begin(ss);
+    std::istream_iterator<std::string> end;
+    std::istream_iterator<std::string> i = begin;
+    std::vector<const char*> itemcstrs;
+    std::vector<std::string> itemstrs;
+    while (i != end) {
+      itemstrs.push_back(*i);
+      ++i;
+    }
+    
+    for (unsigned idx = 0; idx < itemstrs.size(); idx++) {
+      // note: if itemstrs is modified after this, itemcstrs will be full
+      // of invalid pointers! Could make copies, but would have to clean up then...
+      itemcstrs.push_back(itemstrs[idx].c_str());
+    }
 
     auto filter = llvm::MemoryBuffer::getMemBufferCopy(fullfile);
 
-    sourceManager.setMainFileID(sourceManager.createFileID(std::move(filter)));
+    clang::CompilerInvocation::CreateFromArgs(compilerInvocation, itemcstrs.data(), itemcstrs.data() + itemcstrs.size(),
+     *diagnosticsEngine);
 
-    clang::IdentifierTable identifierTable(languageOptions);
-    clang::SelectorTable selectorTable;
-
-    clang::Builtin::Context builtinContext;
-    builtinContext.InitializeTarget(*targetInfo, nullptr);
-    clang::ASTContext astContext(languageOptions,
-                                 sourceManager,
-                                 identifierTable,
-                                 selectorTable,
-                                 builtinContext);
-    astContext.InitBuiltinTypes(*targetInfo);
-    compInst.setTarget(targetInfo.get());
+    auto* languageOptions = compilerInvocation.getLangOpts();
+    auto& preprocessorOptions = compilerInvocation.getPreprocessorOpts();
+    auto& targetOptions = compilerInvocation.getTargetOpts();
+    auto& frontEndOptions = compilerInvocation.getFrontendOpts();
+    frontEndOptions.Inputs.clear();
+    frontEndOptions.Inputs.push_back(clang::FrontendInputFile(filter.get(), clang::InputKind::CXX));
+    
+    targetOptions.Triple = llvm::sys::getDefaultTargetTriple();
+    compilerInstance.createDiagnostics(textDiagnosticPrinter.get(), false);
 
     llvm::LLVMContext context;
     std::unique_ptr<clang::CodeGenAction> action = std::make_unique<clang::EmitLLVMAction>(&context);
     
-    textDiagnosticPrinter->BeginSourceFile(languageOptions, &preprocessor);
-
-    compInst.ExecuteAction(*action);
+    compilerInstance.ExecuteAction(*action);
 
     std::unique_ptr<llvm::Module> module = action->takeModule();
-    llvm::InitializeNativeTarget();
-    llvm::InitializeNativeTargetAsmPrinter();
-    llvm::InitializeNativeTargetAsmParser();
     
     llvm::EngineBuilder builder(std::move(module));
     builder.setMCJITMemoryManager(std::make_unique<llvm::SectionMemoryManager>());
