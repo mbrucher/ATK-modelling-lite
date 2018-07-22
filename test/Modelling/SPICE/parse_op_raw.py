3e87e256b0b64dadd1d9db6e6c47fcca790b5edd
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-07-22 18:58:03 +0100

    Remove lines

--- test/Modelling/SPICE/parse_op_raw.py
+++ test/Modelling/SPICE/parse_op_raw.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 
 from ltspicereader import LTSpiceRawRead
-from matplotlib import pyplot as plt
-import numpy as np
 
 state = {}
 
 raw = LTSpiceRawRead("moog.op.raw")
 for name in raw.get_trace_names():
-    print(name)
     state[name] = raw.get_trace(str(name)).data[0]
 
 print state
