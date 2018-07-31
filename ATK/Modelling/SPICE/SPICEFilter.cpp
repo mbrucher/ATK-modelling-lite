ea5374a80a763242bc75d06a3fc75f7c0c033f1d
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-07-31 21:50:43 +0100

    Refactored common code, build the filter from the AST.

--- ATK/Modelling/SPICE/SPICEFilter.cpp
+++ ATK/Modelling/SPICE/SPICEFilter.cpp
@@ -8,11 +8,26 @@
 #include <ATK/Core/Utilities.h>
 
 #include <ATK/Modelling/SPICE/SPICEFilter.h>
+#include <ATK/Modelling/SPICE/parser.h>
 #include <ATK/Modelling/ModellerFilter.h>
 
 namespace ATK
 {
-
+namespace
+{
+template<typename DataType>
+std::unique_ptr<ModellerFilter<DataType>> convert(const ast::SPICEAST& tree)
+{
+  int nb_dynamic_pins = 0;
+  int nb_static_pins = 0;
+  int nb_input_pins = 0;
+  
+  auto filter = std::make_unique<ModellerFilter<DataType>>(nb_dynamic_pins, nb_static_pins, nb_input_pins);
+  
+  return std::move(filter);
+}
+}
+  
 template<typename DataType>
 std::unique_ptr<ModellerFilter<DataType>> parse(const std::string& filename)
 {
@@ -21,31 +36,23 @@ std::unique_ptr<ModellerFilter<DataType>> parse(const std::string& filename)
   {
     throw ATK::RuntimeError("Cannot open file for reading.");
   }
-  
+
+  ast::SPICEAST tree;
+
   std::string line; // Maybe I should merge lines that start with a '+' before parsing them
   while (std::getline(infile, line))
   {
   }
-  
-  int nb_dynamic_pins = 0;
-  int nb_static_pins = 0;
-  int nb_input_pins = 0;
 
-  auto filter = std::make_unique<ModellerFilter<DataType>>(nb_dynamic_pins, nb_static_pins, nb_input_pins);
-  
-  return std::move(filter);
+  return convert<DataType>(tree);
 }
 
 template<typename DataType>
 std::unique_ptr<ModellerFilter<DataType>> parseStrings(const std::string& strings)
 {
-  int nb_dynamic_pins = 0;
-  int nb_static_pins = 0;
-  int nb_input_pins = 0;
-    
-  auto filter = std::make_unique<ModellerFilter<DataType>>(nb_dynamic_pins, nb_static_pins, nb_input_pins);
-    
-  return std::move(filter);
+  ast::SPICEAST tree;
+
+  return convert<DataType>(tree);
 }
 
 template ATK_MODELLING_EXPORT std::unique_ptr<ModellerFilter<double>> parse<double>(const std::string& filename);
