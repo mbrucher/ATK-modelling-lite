87d2a97bab1242ca038c97643d3a4c1faa095cc8
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-10-13 17:16:29 +0100

    Adding the simple bridge divider, works like a charm.

--- test/Modelling/SPICE/SPICEHandler.cpp
+++ test/Modelling/SPICE/SPICEHandler.cpp
@@ -2,6 +2,8 @@
  * \ file SPICEHandler.cpp
  */
 
+#include <ATK/Core/InPointerFilter.h>
+
 #include <ATK/Modelling/SPICE/SPICEHandler.h>
 #include <ATK/Modelling/Capacitor.h>
 #include <ATK/Modelling/Coil.h>
@@ -13,6 +15,10 @@
 #define BOOST_TEST_NO_MAIN
 
 #include <boost/test/unit_test.hpp>
+
+#include <boost/math/constants/constants.hpp>
+
+constexpr gsl::index PROCESSSIZE = 1024 * 64;
 
 BOOST_AUTO_TEST_CASE( SPICE_Handler_wrong_voltage )
 {
@@ -173,7 +179,7 @@ BOOST_AUTO_TEST_CASE( SPICE_Handler_coil_2 )
   BOOST_CHECK_THROW(handler.process(), ATK::RuntimeError);
 }
 
-BOOST_AUTO_TEST_CASE( SPICE_Handler_bridge )
+BOOST_AUTO_TEST_CASE( SPICE_Handler_simple_bridge )
 {
   ATK::ast::SPICEAST ast;
   BOOST_CHECK_NO_THROW(ATK::parse_string(ast, "Vcc ref 0 5V"));
@@ -185,4 +191,36 @@ BOOST_AUTO_TEST_CASE( SPICE_Handler_bridge )
   filter->process(1);
   auto output = filter->get_output_array(0);
   BOOST_CHECK_EQUAL(output[0], 2.5);
+}
+
+BOOST_AUTO_TEST_CASE( SPICE_Handler_divider_bridge )
+{
+  ATK::ast::SPICEAST ast;
+  BOOST_CHECK_NO_THROW(ATK::parse_string(ast, "Vcc ref 0 5V"));
+  BOOST_CHECK_NO_THROW(ATK::parse_string(ast, "R0 1 0 100"));
+  BOOST_CHECK_NO_THROW(ATK::parse_string(ast, "R1 1 ref 100"));
+  BOOST_CHECK_NO_THROW(ATK::parse_string(ast, "Vin in 0 AC 5V"));
+  BOOST_CHECK_NO_THROW(ATK::parse_string(ast, "R2 in 1 100"));
+  
+  std::unique_ptr<ATK::ModellerFilter<double>> filter = ATK::SPICEHandler<double>::convert(ast);
+  filter->set_input_sampling_rate(48000);
+  filter->set_output_sampling_rate(48000);
+  
+  std::vector<double> data(PROCESSSIZE);
+  for(ptrdiff_t i = 0; i < PROCESSSIZE; ++i)
+  {
+    data[i] = std::sin(2 * boost::math::constants::pi<double>() * (i+1.)/48000 * 1000);
+  }
+  ATK::InPointerFilter<double> generator(data.data(), 1, PROCESSSIZE, false);
+  generator.set_output_sampling_rate(48000);
+
+  filter->set_input_port(0, generator, 0);
+  
+  filter->process(PROCESSSIZE);
+  auto output = filter->get_output_array(0);
+  
+  for(gsl::index i = 0; i < PROCESSSIZE; ++i)
+  {
+    BOOST_CHECK_CLOSE(output[i], (5 + data[i]) / 3, 0.001);
+  }
 }
