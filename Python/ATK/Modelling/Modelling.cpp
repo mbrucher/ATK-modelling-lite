3b08d3733af765701ac990f19456e8ea5a93a361
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-10-26 16:44:57 +0100

    Starting the tests, make Python install work

--- Python/ATK/Modelling/Modelling.cpp
+++ Python/ATK/Modelling/Modelling.cpp
@@ -14,4 +14,7 @@ namespace
 PYBIND11_MODULE(PythonModelling, m)
 {
   m.doc() = "Audio ToolKit Modelling module";
+  
+  py::object f1 = (py::object) py::module::import("ATK.Core").attr("FloatTypedBaseFilter");
+  py::object f2 = (py::object) py::module::import("ATK.Core").attr("DoubleTypedBaseFilter");
 }
