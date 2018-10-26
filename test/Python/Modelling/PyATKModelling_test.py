fc17016fe4e536c2f953091a4321bf72fc7e9b11
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-10-26 18:27:38 +0100

    Now we can create and make some measures on dynamic filters (to be done really soon)

--- test/Python/Modelling/PyATKModelling_test.py
+++ test/Python/Modelling/PyATKModelling_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from ATK.Core import DoubleInPointerFilter, DoubleOutPointerFilter
-from ATK.Modelling import AST
+from ATK.Modelling import AST, DoubleModellerFilter
 
 def createAST_test():
     ast = AST()
@@ -9,3 +9,7 @@ def createAST_test():
 def parseSimpleAST_test():
     ast = AST()
     ast.parse_string("R0 out ref1 200e3")
+
+def createDynamic_test():
+    ast = AST()
+    filter = DoubleModellerFilter.create_dynamic_filter(ast)
